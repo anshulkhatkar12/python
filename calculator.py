afd65d9d84b646a78ca3500543c70c53e9fed23b
Anshul <anshulkhatkar512@gmail.com>
2026-07-24 22:01:13 +0530

    calci

---
+++ calculator.py
@@ -0,0 +1,17 @@
+x= float (input ("x:"))
+y= float (input ("y:"))
+print (x+y)
+z= round (x/y,2)
+print (z)
+
+def main():
+     x= int (input("x:"))
+     print ("square of x is", square(x))
+
+def square(x):
+    return x*x
+    #or we can also use x**2
+main ()
+
+
+
