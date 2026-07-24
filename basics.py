ac40b744e12ab16e9bd63551e18c9e66cf4588ad
Anshul <anshulkhatkar512@gmail.com>
2026-07-24 18:05:01 +0530

    basics_1

--- basics.py
+++ basics.py
@@ -1,1 +1,19 @@
 print("hello world")
+#asking for name, removing whitespace and capitalizing first letter  
+name = input ("whats your name? ").strip().title()
+#concatenation
+print ("hello, " + name)
+print("hello,",name)
+#seperating 2 strings
+print("hello,", name, sep='yo')  
+#using ""inside a function
+print ("hello, \"friend\"")
+#special string 
+print(f"hello,{name}")
+#split users name into first and last name 
+first, last = name.split()
+print ("hello," , last)
+
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+print("hello world")
+name = input ("whats your name? ")
+print ("hello, " + name)
+print("hello,",name)
+  
+print("hello,", name, sep='')  
