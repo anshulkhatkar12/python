604d82f5d613c7c78bc109b87991a6dd71e340fa
Anshul <anshulkhatkar512@gmail.com>
2026-08-03 23:02:41 +0530

    cowsay

--- library.py
+++ library.py
@@ -1,15 +1,5 @@
+import cowsay
 import sys
-if len(sys.argv) < 4:
-    print("few arguments")
-elif len(sys.argv) > 4:
-    print("too many arguments")
-else:
-    print ("welcome", sys.argv[1])
-    print ("youre", sys.argv[2], "years old")
-    print ("you live in", sys.argv[3])
-for arg in sys.argv[1:]:
-    print (arg)
-
-
-
-    
+cowsay.daemon("Hello!")
+cowsay.cow("Hello!")
+cowsay.tux("Hello!")
